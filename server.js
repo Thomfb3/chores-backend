64d4f37928024da014430505a200ddf0345b7fff
Thomas Balunis <thomfb3@gmail.com>
2021-10-29 00:57:37 -0400

    Mongoose connection removed

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ dotenv.config({ path: "./config.env" });
 const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
 
 
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false
-}).then(con => {
-  console.log(con.connections);
-  console.log("DB Connection successful")
-});
+// mongoose.connect(DB, {
+//   useNewUrlParser: true,
+//   // useCreateIndex: true,
+//   // useFindAndModify: false
+// }).then(con => {
+//   console.log(con.connections);
+//   console.log("DB Connection successful")
+// });
 
 
 
