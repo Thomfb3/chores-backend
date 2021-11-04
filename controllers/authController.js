0539004afbab785af558af6f56ff52cb6bcf3587
Thomas Balunis <thomfb3@gmail.com>
2021-11-04 00:31:17 -0400

    authController fixes

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,53 +8,52 @@ const catchAsync = require('../helpers/catchAsync');
  *  Authorization required: none **/
 exports.authenticateAndGetToken = catchAsync(async (req, res, next) => {
     try {
-        ///Get Username, password from request
         const { username, password } = req.body;
-
-        //authenticate the username password and team
+        //authenticate the username and password
         const user = await User.findOne({ username: username });
         const authenticated = await user.authenticate(password, user.password);
 
-
         if (authenticated) {
-            //Create token
+            //Return token with username and team if user has team
             const token = createToken(user);
             return res.json({ token });
-        }
+        };
     } catch (err) {
         return next(err);
     };
- });
+});
 
 /** Returns JWT token which can be used to authenticate further requests.
  *  Authorization required: none **/
 exports.registerAndGetToken = catchAsync(async (req, res, next) => {
     try {
         const newUser = await User.create({ ...req.body, role: "user" });
-
         const token = createToken(newUser);
+        //Return token token with username and no team
         return res.status(201).json({ token });
-
     } catch (err) {
         return next(err);
     };
- });
+});
 
 /* Returns JWT token which can be used to authenticate further requests.
  * Authorization required: user must be logged in **/
 exports.createTeamAndGetToken = catchAsync(async (req, res, next) => {
     try {
         const { teamName, teamPassword, username } = req.body;
-
-        //Find user, create team, add team to user and add user to team
+        //Find user
         const user = await User.findOne({ username: username });
+        if (!user) {
+            throw new BadRequestError("Username not found", 401);
+        }
+        //Create team and add user to team
         const newTeam = await Team.create({ name: teamName, password: teamPassword, users: [user] });
+        //Add team to user and make user admin
         const updatedUser = await User.findByIdAndUpdate({ _id: user._id },
-            { $set: { team: newTeam, role: "admin" } });
-
+            { $set: { teamId: newTeam, role: "admin" } });
+        //Return token with teamId now added
         const token = createToken(updatedUser);
         return res.status(201).json({ token });
-
     } catch (err) {
         return next(err);
     };
@@ -65,19 +64,24 @@ exports.createTeamAndGetToken = catchAsync(async (req, res, next) => {
 exports.joinTeamAndGetToken = catchAsync(async (req, res, next) => {
     try {
         const { teamName, teamPassword, username } = req.body;
+        //Find team
         const foundTeam = await Team.findOne({ name: teamName })
+        if (!foundTeam) {
+            throw new BadRequestError("Team not found", 401)
+        }
+        //Authenticate teamName and  teamPassword
         const authenticated = await foundTeam.authenticate(teamPassword, foundTeam.password);
-
         if (authenticated) {
+            //Add team to user
             const updatedUser = await User.findOneAndUpdate({ username: username },
-                { $set: { team: foundTeam } });
-            
+                { $set: { teamId: foundTeam } });
             if (!updatedUser) {
                 throw new BadRequestError("No user found", 401);
             };
-
+            //Add user to team
             await Team.findByIdAndUpdate({ _id: foundTeam._id },
                 { $addToSet: { users: updatedUser } });
+            //Return token with teamId now added
             const token = createToken(updatedUser);
             return res.status(201).json({ token });
         } else {
