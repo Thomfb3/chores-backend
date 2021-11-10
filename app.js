e3045eba6bcf1e4cf0368707fc95ba8fa6654ed2
Thomas Balunis <thomfb3@gmail.com>
2021-11-09 23:48:29 -0500

    cors config

--- app.js
+++ app.js
@@ -18,8 +18,15 @@ const commentRoutes = require("./routes/comments");
 const morgan = require("morgan");
 const app = express();
 
-app.use(cors());
-app.options('*', cors());
+// app.use(cors());
+// app.options('*', cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json());
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
